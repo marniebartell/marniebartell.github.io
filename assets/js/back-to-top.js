b8116ca73ba03c15df42695224d8e2da93fe1cef
marniebartell <marnie.bartell@gmail.com>
2025-03-04 20:57:03 -0700

    Update back-to-top.js

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,1 +1,19 @@
+// Back to top button
+let backbutton = document.getElementById("back-to-top");
 
+// Show button when scrolling 350px down
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    backbutton.style.display = "block";
+  } else {
+    backbutton.style.display = "none";
+  }
+}
+
+// Scroll to the top when button clicked
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
