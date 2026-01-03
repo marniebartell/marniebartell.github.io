d7f51de3305de4986efa918788033130d256bae3
marniebartell <marnie.bartell@gmail.com>
2026-01-02 17:29:28 -0700

    Update back-to-top.js

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -17,3 +17,6 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Update year
+  document.getElementById("currentYear").innerText = new Date().getFullYear();
